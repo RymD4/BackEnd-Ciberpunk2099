b7272d115bd0cef4ab1aaf865256da625ebe54c3
Rodrigo Raymundo <rodrigo.rym.24@gmail.com>
2023-11-20 00:13:13 -0500

    feat add CategoryService
    Se creo el servicio para category

---
+++ BackEnd-Ciberpunk2099/Services/CategoryRepository.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BackEnd_Ciberpunk2099.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class CategoryRepository
+{
+    private readonly CiberPunk2099Context _context;
+
+    public CategoryRepository(CiberPunk2099Context context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
+    {
+        return await _context.Categories.ToListAsync();
+    }
+
+    public async Task<Category> GetCategoryByIdAsync(int categoryId)
+    {
+        return await _context.Categories.FindAsync(categoryId);
+    }
+
+    public async Task AddCategoryAsync(Category category)
+    {
+        _context.Categories.Add(category);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateCategoryAsync(Category category)
+    {
+        _context.Entry(category).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteCategoryAsync(int categoryId)
+    {
+        var category = await _context.Categories.FindAsync(categoryId);
+        if (category != null)
+        {
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ BackEnd-Ciberpunk2099/Services/CategoryService.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BackEnd_Ciberpunk2099.Models;
+
+public class CategoryService
+{
+    private readonly CategoryRepository _categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+
+    public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
+    {
+        return await _categoryRepository.GetAllCategoriesAsync();
+    }
+
+    public async Task<Category> GetCategoryByIdAsync(int categoryId)
+    {
+        return await _categoryRepository.GetCategoryByIdAsync(categoryId);
+    }
+
+    public async Task AddCategoryAsync(Category category)
+    {
+        await _categoryRepository.AddCategoryAsync(category);
+    }
+
+    public async Task UpdateCategoryAsync(Category category)
+    {
+        await _categoryRepository.UpdateCategoryAsync(category);
+    }
+
+    public async Task DeleteCategoryAsync(int categoryId)
+    {
+        await _categoryRepository.DeleteCategoryAsync(categoryId);
+    }
+}
