9511c8f0aae6179cac5afc4e9c96ab1a1d09b7d7
Rodrigo Raymundo <rodrigo.rym.24@gmail.com>
2023-11-20 00:32:56 -0500

    feat Add OrderService
    se agrego orderservice y orderrepository

---
+++ BackEnd-Ciberpunk2099/Services/OrderRepository.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BackEnd_Ciberpunk2099.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class OrderRepository
+{
+    private readonly CiberPunk2099Context _context;
+
+    public OrderRepository(CiberPunk2099Context context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Order>> GetAllOrdersAsync()
+    {
+        return await _context.Orders.ToListAsync();
+    }
+
+    public async Task<Order> GetOrderByIdAsync(int orderId)
+    {
+        return await _context.Orders.FindAsync(orderId);
+    }
+
+    public async Task AddOrderAsync(Order order)
+    {
+        _context.Orders.Add(order);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateOrderAsync(Order order)
+    {
+        _context.Entry(order).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteOrderAsync(int orderId)
+    {
+        var order = await _context.Orders.FindAsync(orderId);
+        if (order != null)
+        {
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ BackEnd-Ciberpunk2099/Services/OrderService.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BackEnd_Ciberpunk2099.Models;
+
+public class OrderService
+{
+    private readonly OrderRepository _orderRepository;
+
+    public OrderService(OrderRepository orderRepository)
+    {
+        _orderRepository = orderRepository;
+    }
+
+    public async Task<IEnumerable<Order>> GetAllOrdersAsync()
+    {
+        return await _orderRepository.GetAllOrdersAsync();
+    }
+
+    public async Task<Order> GetOrderByIdAsync(int orderId)
+    {
+        return await _orderRepository.GetOrderByIdAsync(orderId);
+    }
+
+    public async Task AddOrderAsync(Order order)
+    {
+        await _orderRepository.AddOrderAsync(order);
+    }
+
+    public async Task UpdateOrderAsync(Order order)
+    {
+        await _orderRepository.UpdateOrderAsync(order);
+    }
+
+    public async Task DeleteOrderAsync(int orderId)
+    {
+        await _orderRepository.DeleteOrderAsync(orderId);
+    }
+}
