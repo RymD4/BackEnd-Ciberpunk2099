61638f965ffc4b4f7e43902dd432ab34bc3ddc4f
Rodrigo Raymundo <rodrigo.rym.24@gmail.com>
2023-11-20 00:19:34 -0500

    Update ProductRepository.cs

--- BackEnd-Ciberpunk2099/Services/ProductRepository.cs
+++ BackEnd-Ciberpunk2099/Services/ProductRepository.cs
@@ -44,4 +44,29 @@ public class ProductRepository
         }
     }
 
+    internal async Task<List<Product>> GetProductsAsync()
+    {
+        return await _context.Products.ToListAsync();
+    }
+
+    internal async Task CreateProductAsync(Product product)
+    {
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+    }
+
+    internal async Task UpdateProductAsync(int productId, Product updatedProduct)
+    {
+        var existingProduct = await _context.Products.FindAsync(productId);
+
+        if (existingProduct != null)
+        {
+            existingProduct.Title = updatedProduct.Title;
+            existingProduct.Description = updatedProduct.Description;
+            existingProduct.Price = updatedProduct.Price;
+            existingProduct.Category = updatedProduct.Category;
+
+            await _context.SaveChangesAsync();
+        }
+    }
 }
