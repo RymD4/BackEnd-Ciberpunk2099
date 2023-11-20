0f9ec42eda7216d81d5983dcf97c9a82c2adf5bb
Rodrigo Raymundo <130820365+RodrigoRG24@users.noreply.github.com>
2023-11-20 00:03:30 -0500

    feat AddProductService
    se agrego el servicio de productos

---
+++ BackEnd-Ciberpunk2099/Services/ProductRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BackEnd_Ciberpunk2099.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class ProductRepository
+{
+    private readonly CiberPunk2099Context _context;
+
+    public ProductRepository(CiberPunk2099Context context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Product>> GetAllProductsAsync()
+    {
+        return await _context.Products.ToListAsync();
+    }
+
+    public async Task<Product> GetProductByIdAsync(int productId)
+    {
+        return await _context.Products.FindAsync(productId);
+    }
+
+    public async Task AddProductAsync(Product product)
+    {
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateProductAsync(Product product)
+    {
+        _context.Entry(product).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteProductAsync(int productId)
+    {
+        var product = await _context.Products.FindAsync(productId);
+        if (product != null)
+        {
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+}
---
+++ BackEnd-Ciberpunk2099/Services/ProductService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BackEnd_Ciberpunk2099.Models;
+
+namespace BackEnd_Ciberpunk2099.Services
+{
+    public class ProductService
+    {
+        private readonly ProductRepository _productRepository;
+
+        public ProductService(ProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+        }
+
+        public async Task<List<Product>> GetProductsAsync()
+        {
+            return await _productRepository.GetProductsAsync();
+        }
+
+        public async Task<Product> GetProductByIdAsync(int productId)
+        {
+            return await _productRepository.GetProductByIdAsync(productId);
+        }
+
+        public async Task CreateProductAsync(Product product)
+        {
+            if (product == null)
+                throw new ArgumentNullException(nameof(product));
+
+            await _productRepository.CreateProductAsync(product);
+        }
+
+        public async Task UpdateProductAsync(int productId, Product updatedProduct)
+        {
+            if (updatedProduct == null)
+                throw new ArgumentNullException(nameof(updatedProduct));
+
+            await _productRepository.UpdateProductAsync(productId, updatedProduct);
+        }
+
+        public async Task DeleteProductAsync(int productId)
+        {
+            await _productRepository.DeleteProductAsync(productId);
+        }
+    }
+}
